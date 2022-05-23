5faef631bebb08fd0fe77d6a2e10027dcf839dcc
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-05-23 19:25:52 +0100

    Add palindrome exercise

---
+++ exercises/palindrome.py
@@ -0,0 +1,18 @@
+# Determine if a given string is a palindrome
+
+def palindrome(input):
+  """
+  Return True if a given string is a palindrome, False otherwise
+  """
+
+  if (len(input) <= 1): return True;
+
+  half_length = len(input) // 2
+
+  index = 0
+
+  while (index < half_length):
+    if (input[index] != input[len(input) - 1 - index]): return False
+    index += 1
+
+  return True
