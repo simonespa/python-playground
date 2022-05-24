6190769e0723052de281e845a0a3180dc34ed9d0
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-05-24 08:59:42 +0100

    Code formatted exercises folder

--- exercises/fibonacci.py
+++ exercises/fibonacci.py
@@ -1,8 +1,11 @@
-def fibonacci(n, memo = {}):
-  if (n in memo): return memo[n]
-  if (n <= 0): return 0
-  if (n <= 2): return 1
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 0:
+        return 0
+    if n <= 2:
+        return 1
 
-  memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
 
-  return memo[n]
+    return memo[n]
--- exercises/palindrome.py
+++ exercises/palindrome.py
@@ -1,18 +1,21 @@
 # Determine if a given string is a palindrome
 
+
 def palindrome(input):
-  """
-  Return True if a given string is a palindrome, False otherwise
-  """
+    """
+    Return True if a given string is a palindrome, False otherwise
+    """
 
-  if (len(input) <= 1): return True;
+    if len(input) <= 1:
+        return True
 
-  half_length = len(input) // 2
+    half_length = len(input) // 2
 
-  index = 0
+    index = 0
 
-  while (index < half_length):
-    if (input[index] != input[len(input) - 1 - index]): return False
-    index += 1
+    while index < half_length:
+        if input[index] != input[len(input) - 1 - index]:
+            return False
+        index += 1
 
-  return True
+    return True
