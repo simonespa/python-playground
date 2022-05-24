cf57320c130c0d1845747cf9e2008ddffe689352
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-05-24 09:25:36 +0100

    Fix https://deepsource.io/gh/simonespa/python-playground/issue/PYL-W0622/occurrences

--- exercises/palindrome.py
+++ exercises/palindrome.py
@@ -1,20 +1,19 @@
 # Determine if a given string is a palindrome
 
-
-def palindrome(input):
+def palindrome(input_string):
     """
     Return True if a given string is a palindrome, False otherwise
     """
 
-    if len(input) <= 1:
+    if len(input_string) <= 1:
         return True
 
-    half_length = len(input) // 2
+    half_length = len(input_string) // 2
 
     index = 0
 
     while index < half_length:
-        if input[index] != input[len(input) - 1 - index]:
+        if input_string[index] != input_string[len(input_string) - 1 - index]:
             return False
         index += 1
 
