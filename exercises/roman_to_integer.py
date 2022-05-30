22feed9ffe82cda5efb593fd5525e67f8fd2f51f
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-05-30 12:43:59 +0100

    Code formatting

--- exercises/roman_to_integer.py
+++ exercises/roman_to_integer.py
@@ -1,12 +1,5 @@
-numbers = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
+numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
 
 def roman_to_integer(roman_number):
     sum = 0
